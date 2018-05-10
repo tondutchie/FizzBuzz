dd79d752b19d09d6574d1dcad3cb942c8576cea4
tondutchie <pumwaree@gmail.com>
2018-05-10 16:11:51 +0700

    moved back fastlane folder

--- FizzBuzzTest/build/FizzBuzz.build/Release-iphoneos/FizzBuzz.build/DerivedSources/FizzBuzz_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char FizzBuzzVersionString[];
- extern const double FizzBuzzVersionNumber;
-
- const unsigned char FizzBuzzVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FizzBuzz  PROJECT:FizzBuzz-3" "\n";
- const double FizzBuzzVersionNumber __attribute__ ((used)) = (double)3.;
--- FizzBuzzTest/build/FizzBuzz.build/Release-iphoneos/FizzBuzz.build/Objects-normal/arm64/FizzBuzz_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/tondutchie/Documents/tdcDocuments/TDC/Coding/FizzBuzzGit/FizzBuzzTest/build/FizzBuzz.build/Release-iphoneos/FizzBuzz.build/DerivedSources/FizzBuzz_vers.c
